788aab563511181bc319e1e681d89a0899d625ac
shandohowell <shandohowell94@gmail.com>
2025-01-07 00:33:08 -0500

    Worked on processing queue.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ConvexClientProvider } from "@/components/convexClientProvider";
 import SyncUserWithConvex from "@/components/SyncUserWithConvex";
 import Header from "@/components/Header";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Ticketz",
@@ -24,6 +25,7 @@ export default function RootLayout({
             <Header/>
             <SyncUserWithConvex />
             {children}
+            <Toaster />
           </ClerkProvider>
         </ConvexClientProvider>
       </body>
